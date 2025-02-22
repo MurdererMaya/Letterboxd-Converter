dabe0d3455bf0e5e109c0c2ee69ba3138eabf2d1
Maya <gapmanx@outlook.com>
2025-02-21 22:18:01 -0500

    CODE WORKS NOW WOAHHH :O
    conversion works, but i need to remove shows in the conversion so the csv file only has movies

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ output_file = 'letterboxd_import.csv'
 def process_netflix_csv(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8-sig') as infile, open(output_file, 'w', newline='', encoding='utf-8') as outfile:
         reader = csv.reader(infile)
+        next(reader)
         fieldnames = ['Title', 'Year', 'WatchedDate']
         writer = csv.DictWriter(outfile, fieldnames=fieldnames)
 
         writer.writeheader()
         for row in reader:
             title, date = row[0], row[1]
-            year = '' 
-            watched_date = datetime.strptime(date, '%Y-%m-%d').strftime('%Y-%m-%d')
+            year = ''
+            watched_date = datetime.strptime(date, '%m/%d/%y').strftime('%Y-%m-%d')
 
             writer.writerow({'Title': title.strip(), 'Year': year, 'WatchedDate': watched_date})
+
 process_netflix_csv(input_file, output_file)
 
 print(f"Conversion completed. The new file is saved as {output_file}")
